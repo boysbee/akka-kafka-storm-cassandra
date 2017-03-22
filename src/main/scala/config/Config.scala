064e35d39bdd400b0ff5604a4016fd69948e2734
nattapornc <nattaporn.c@dotography.com>
2017-03-22 17:33:00 +0700

    Change default port to `8090`.

--- src/main/scala/config/Config.scala
+++ src/main/scala/config/Config.scala
@@ -5,5 +5,5 @@ object Config {
   val KafkaProducerConfigLocation = "/kafka-0.10.0.1-producer-defaults.properties"
 
   val ServerHost = "0.0.0.0"
-  val ServerPort = 9090
+  val ServerPort = 8090
 }
