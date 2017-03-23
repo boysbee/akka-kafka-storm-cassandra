a5c43889efa6dfbaca270c4ce8a40dfcadafd40f
nattapornc <nattaporn.c@dotography.com>
2017-03-23 16:21:44 +0700

    Change script  to add run with jar and main class.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 JAVA_OPTS=${JAVA_OPTS:="-Xmx128m -Xms128m -server -verbose:gc -XX:+UseG1GC -Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote.port=9090 -Dcom.sun.management.jmxremote.rmi.port=9090 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"}
 
-exec java -jar $JAVA_OPTS /app/server.jar
+exec java -cp /app/server.jar WebServer $JAVA_OPTS
